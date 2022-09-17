eedecc6d356962d81ddc059873cf28ec224c96ec
MewShairos <mewshairoz@gmail.com>
2022-09-17 13:18:43 -0300

    Header + Search - HTML and CSS

--- Recipes/views.py
+++ Recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def sobre(request):
-    return render(request, 'recipes/pages/home.html', context={"name": "Iuri"})
+    return render(request, 'recipes/pages/home.html')
